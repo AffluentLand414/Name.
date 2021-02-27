c9791ef41b6d50d40384015ec56b39167a7bf97c
AffluentLand414 <77493621+AffluentLand414@users.noreply.github.com>
2021-02-27 14:53:11 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,36 @@
+var fixedRect, movingRect;
+var car,wall
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  wall=createSprite(500,900,50,100);
+  wall.shapeColor = "white";
+  car=createSprite(900,400,20,20);
+  car.shapeColor = "blue";
+  car.velocityX=3.3;
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  drawSprites();
+}
